430fc1a1b70a02351ac36ec38a24deef2ca2613d
Tobias Buschor <tobias.buschor@shwups.ch>
2020-10-18 22:37:06 +0200

    vers

--- tests/run.js
+++ tests/run.js
@@ -1,4 +1,4 @@
-import {serve} from "https://deno.land/std@0.50.0/http/server.ts";
+import {serve} from "https://deno.land/std@0.74.0/http/server.ts";
 import likePHP from "../php.js";
 
 let phplike = new likePHP({
